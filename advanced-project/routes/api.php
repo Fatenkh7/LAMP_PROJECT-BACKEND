d1d33d84bc17c013c43a8c02f5f21cfa3fe5f83b
Faten_khoder <Khoderfaten5@gmail.com>
2023-02-23 12:58:54 +0000

    report-routes

--- advanced-project/routes/api.php
+++ advanced-project/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\report_controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,4 +24,10 @@ Route::Post('/admin', [AdminController::class, 'addAdmin']);
 Route::Put('/admin/{id}', [AdminController::class, 'editAdmin']);
 Route::Delete('/admin/{id}', [AdminController::class, 'deleteAdmin']);
 
-
+Route::Get('/report', [report_controller::class, 'getAll']);
+Route::Get('/report/{id}', [report_controller::class, 'getById']);
+Route::Get('/report/{type}', [report_controller::class, 'getByType']);
+Route::Post('/report', [report_controller::class, 'addReport']);
+Route::Put('/report/{id}', [report_controller::class, 'editReport']);
+Route::Delete('/report/{id}', [report_controller::class, 'deleteById']);
+Route::Delete('/report/{type}', [report_controller::class, 'deleteByType']);
