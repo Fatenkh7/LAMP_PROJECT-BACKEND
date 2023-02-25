c36584ee7dddd5bc8d1c96e125a2330a074ccfae
Faten khoder <112199182+Fatenkh7@users.noreply.github.com>
2023-02-25 16:58:50 +0000

    currency validations

--- advanced-project/app/Http/Controllers/CurrencyController.php
+++ advanced-project/app/Http/Controllers/CurrencyController.php
@@ -4,62 +4,123 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Currency;
+use Illuminate\Validation\Rule;
+//for example, if the database connection is lost or...
+use Illuminate\Database\QueryException;
 
 class CurrencyController extends Controller
 {
     public function getAll()
     {
-        $Currency = Currency::all();
-        return response()->json([
-            'message' => $Currency
-        ]);
-    }
-
-    public function getCurrencyById(Request $Request, $id)
-    {
-        $Currency = Currency::find($id);
-        if ($Currency) {
+        try {
+            $currencies = Currency::all();
             return response()->json([
-                'success' => true,
-                'data' => $Currency
+                'message' => $currencies
             ]);
-        } else {
+        } catch (QueryException $e) {
             return response()->json([
                 'success' => false,
-                'message' => 'This currency not found'
+                'message' => 'Error retrieving currencies from database'
+            ]);
+        }
+    }
+
+    public function getCurrencyById(Request $request, $id)
+    {
+        try {
+            $currency = Currency::find($id);
+            if ($currency) {
+                return response()->json([
+                    'success' => true,
+                    'data' => $currency
+                ]);
+            } else {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'This currency not found'
+                ]);
+            }
+        } catch (QueryException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Error retrieving currency from database'
             ]);
         }
     }
 
     public function addCurrency(Request $request)
     {
-        $newCurrency = new Currency();
-        $currency = $request->input('currency');
-        $newCurrency->currency = $currency;
-        $newCurrency->save();
-        return response()->json([
-            'message' => 'Currency created successfully'
-        ]);
+        try {
+            $validatedData = $request->validate([
+                'currency' => ['required', Rule::unique('currencies', 'currency')],
+            ]);
+
+            $currency = new Currency($validatedData);
+            $currency->save();
+
+            return response()->json([
+                'message' => 'Currency created successfully'
+            ]);
+        } catch (QueryException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Error adding currency to database'
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage()
+            ]);
+        }
     }
 
     public function editCurrencyById(Request $request, $id)
     {
-        $Currency = Currency::find($id);
-        $inputs = $request->except('_method');
-        $Currency->update($inputs);
+        try {
+            $validatedData = $request->validate([
+                'currency' => ['required', Rule::unique('currencies', 'currency')->ignore($id)],
+            ]);
 
-        return response()->json([
-            'message' => 'Currency updated successfully',
-            'report' => $Currency,
-        ]);
+            $currency = Currency::find($id);
+            $currency->fill($validatedData);
+            $currency->save();
+
+            return response()->json([
+                'message' => 'Currency updated successfully',
+                'report' => $currency,
+            ]);
+        } catch (QueryException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Error updating currency in database'
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage()
+            ]);
+        }
     }
 
     public function deleteCurrency(Request $request, $id)
     {
-        $Currency = Currency::find($id);
-        $Currency->delete();
-        return response()->json([
-            'message' => 'Currency deleted successfully',
-        ]);
+        try {
+            $currency = Currency::find($id);
+            $currency->delete();
+
+            return response()->json([
+                'message' => 'Currency deleted successfully',
+            ]);
+        } catch (QueryException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Error deleting currency from database'
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage()
+            ]);
+        }
     }
 }
