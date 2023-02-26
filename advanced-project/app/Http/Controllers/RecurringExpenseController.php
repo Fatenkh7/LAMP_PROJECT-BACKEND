0396678e009bda755f133fd30c79ca356b2d0e59
Mazen El Ali <mazenelali>
2023-02-26 13:45:57 +0200

    editttttttt

--- advanced-project/app/Http/Controllers/RecurringExpenseController.php
+++ advanced-project/app/Http/Controllers/RecurringExpenseController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\RecurringExpense;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
 class RecurringExpenseController extends Controller
@@ -9,57 +10,109 @@ class RecurringExpenseController extends Controller
             // add Recurringexpenses
 
     public function addRecurringexpenses(Request $request){
-        $recurringexpenses = new RecurringExpense;
-        $title = $request->input('title');
-        $description = $request->input('description');
-        $amount = $request->input('amount');
-        $startDate = $request->input('startDate');
-        $endDate = $request->input('endDate');
+        try {
+            //validate data
 
-        $recurringexpenses ->title =$title;
-        $recurringexpenses ->description=$description;
-        $recurringexpenses ->amount=$amount;
-        $recurringexpenses ->startDate=$startDate;
-        $recurringexpenses ->endDate=$endDate;
-        $recurringexpenses ->save();
-        return response()->json([
-            'message'=>'create recurringexpenses succes'
-        ]);
+            $data = $request->only('title', 'description', 'amount', 'startDate', 'endDate');
+            $validator = Validator::make($data, [
+                'title' => 'required|string|min:3|max:255',
+                'description' => 'required|string|min:3|max:255',
+                'amount' => 'required|integer',
+                'startDate' => 'required|date_format:Y-m-d',
+                'endDate' => 'required|date_format:Y-m-d',
+
+            ]);
+            if ($validator->fails()) {
+                $errors = $validator->errors()->toArray();
+                return $errors;
+            }
+
+            $recurringexpenses = new RecurringExpense;
+            $title = $request->input('title');
+            $description = $request->input('description');
+            $amount = $request->input('amount');
+            $startDate = $request->input('startDate');
+            $endDate = $request->input('endDate');
+
+            $recurringexpenses->title = $title;
+            $recurringexpenses->description = $description;
+            $recurringexpenses->amount = $amount;
+            $recurringexpenses->startDate = $startDate;
+            $recurringexpenses->endDate = $endDate;
+            $recurringexpenses->save();
+            return response()->json([
+                'message' => 'create recurringexpenses succes'
+            ]);
+        } catch (\Exception $e) {
+            return $e->getMessage();
+        }
 }
 // get all Recurringexpenses
 public function getallRecurringexpenses(Request $request){
-    $recurringexpenses = RecurringExpense::all();
+    try{
+        $recurringexpenses = RecurringExpense::all();
+
+        return response()->json([
+            'message' => $recurringexpenses
+        ]);
+    }catch(\Exception $e){
+        return$e->getMessage();
+    }
     
-    return response()->json([
-        'message'=>$recurringexpenses
-    ]);
 }
 // get by id Recurringexpenses
 public function getByIDRecurringexpenses($id){
-    $recurringexpenses = RecurringExpense::find($id);
-    
-    return response()->json([
-        'message'=>$recurringexpenses
-    ]);
+    try{
+        $recurringexpenses = RecurringExpense::find($id);
+
+        return response()->json([
+            'message' => $recurringexpenses
+        ]);
+    }catch (\Exception $e){
+        return $e->getMessage();
+    }
 }
 // edit Recurringexpenses
 public function editRecurringexpenses(Request $request , $id){
+    try{
+        //validation data
+        $data = $request->only('title','description','amount','startDate','endDate');
+        $validator = Validator::make($data,[
+            'title' => 'required|string|min:3|max:255',
+            'description' => 'required|string|min:3|max:255',
+            'amount' => 'required|integer',
+            'startDate' => 'required|date_format:Y-m-d',
+            'endDate' => 'required|date_format:Y-m-d',
+        ]);
+        if($validator ->fails()){
+            $errors= $validator->errors()->toArray();
+            return $errors;
+        }
+
+
     $recurringexpenses = RecurringExpense::find($id);
     $inputs = $request->except('_method');
-    $recurringexpenses -> update($inputs);
+    $recurringexpenses->update($inputs);
 
     return response()->json([
-        'message'=> 'update recurringexpenses succes'
-    ]);
+        'message' => 'update recurringexpenses succes'
+    ]);}
+    catch(\Exception $e){
+        return $e->getMessage();
+    }
 }
 // delete Recurringexpenses
-public function deleteRecurringexpenses(Request $request ,$id){
-$recurringexpenses = RecurringExpense::find($id);
+public function deleteRecurringexpenses($id){
+try{
+    $recurringexpenses = RecurringExpense::find($id);
 
-$recurringexpenses -> delete();
+    $recurringexpenses->delete();
 
-return response()->json([
-    'message'=>'delete succs',
-]);
+    return response()->json([
+        'message' => 'delete succs',
+    ]);}
+    catch(\Exception $e){
+        return $e->getMessage();
+    }
 }
 }
