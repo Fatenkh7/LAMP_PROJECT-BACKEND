baad0090c9dad512c983879c469e1da158c27865
Faten khoder <112199182+Fatenkh7@users.noreply.github.com>
2023-03-01 22:13:35 +0000

    the re-generate on monthly

--- advanced-project/app/Console/Kernel.php
+++ advanced-project/app/Console/Kernel.php
@@ -32,8 +32,35 @@ class Kernel extends ConsoleKernel
         require base_path('routes/console.php');
     }
 
-    protected function generateMonthlyTransactions()
-    {
-        // Your code for generating monthly transactions goes here
+/**
+ * Generate monthly transactions.
+ */
+protected function generateMonthlyTransactions()
+{
+    $transactions = [];
+
+    // Get all fixed transactions that are scheduled monthly
+    $fixedTransactions = FixedTransaction::where('schedule', 'monthly')->get();
+
+    // For each fixed transaction, generate a transaction with the fixed amount
+    foreach ($fixedTransactions as $fixedTransaction) {
+        $transaction = [
+            'title' => $fixedTransaction->title,
+            'description' => $fixedTransaction->description,
+            'amount' => $fixedTransaction->amount,
+            'date_time' => now(),
+            'type' => $fixedTransaction->type,
+            'schedule' => 'monthly',
+            'is_paid' => false,
+            'fixed_keys_id' => $fixedTransaction->fixed_keys_id,
+            'currencies_id' => $fixedTransaction->currencies_id,
+            'admins_id' => $fixedTransaction->admins_id,
+            'categories_id' => $fixedTransaction->categories_id,
+        ];
+        array_push($transactions, $transaction);
     }
+
+    return $transactions;
+}
+
 }
